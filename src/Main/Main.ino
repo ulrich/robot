68df1b989d07bc94a1efa9bd248c6a99f39b61ce
Guillaume Rose <guillaume.rose@mappy.com>
2014-10-31 14:01:35 +0100

    Asservissement sur place

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -1,57 +1,120 @@
 #include <Encoder.h>
 
-Encoder myEnc(7, 8);
+Encoder leftEncoder(5, 6);
+Encoder rightEncoder(7, 8);
 
-int vitesse = 50;
+IntervalTimer timer;
+
 void setup() {
-   pinMode(9, OUTPUT);
-   pinMode(10, OUTPUT);
-   pinMode(11, OUTPUT);
-   pinMode(12, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
 
-   analogWriteFrequency(9, 16000);
-   analogWriteFrequency(10, 16000);
+    analogWriteFrequency(9, 16000);
+    analogWriteFrequency(10, 16000);
 
-   Serial.begin(9600);
-   Serial.println("Basic Encoder Test:");
-   // left
-   analogWrite(9, vitesse);
-   digitalWrite(11, HIGH);
+    Serial.begin(115200);
 
-   // right
-   analogWrite(10, vitesse);
-   digitalWrite(12, HIGH);
+    analogWrite(9, 0);
+    digitalWrite(11, LOW);
+    analogWrite(10, 0);
+    digitalWrite(12, LOW);
+
+    timer.begin(refresh, 1000);
 }
 
-long oldPosition  = -999;
+int32_t dist_right, dist_left, cmd_right, cmd_left, target_dist, target_angle;
 
 void loop() {
-   /*  long newPosition = myEnc.read();
-   if (newPosition != oldPosition) {
-     oldPosition = newPosition;
-     Serial.println(newPosition);
+    while (Serial.available() == 0) {
+        ;
+    }
 
-     double speed = (5000 - newPosition);
-     Serial.print("p= ");
-     Serial.print(newPosition);
-     Serial.print(", speed= ");
-     Serial.println(speed);
+    long int i;
+    unsigned char premierCaractere = Serial.read();
+    switch (premierCaractere) {
+        case 'a':
+            litEntierLong(&i);
+            target_dist = i;
+            Serial.println(target_dist);
+            break;
+        case 'b':
+            litEntierLong(&i);
+            target_angle = i;
+            Serial.println(target_angle);
+            break;
+        default:
+            break;
+    }
+}
 
-     if (speed > 0) {
-       analogWrite(10, speed > 60 ? 60 : speed);
-       digitalWrite(11, LOW);
-     } else {
-       analogWrite(10, -speed > 60 ? 60 : - speed);
-       digitalWrite(11, HIGH);
-     }*/
+void refresh() {
+    int32_t dist, cmd_dist, angle, cmd_angle, err_dist, err_angle;
 
-	if (Serial.available() > 0) {
-		int incomingByte = Serial.parseInt();
-		Serial.print("USB received: ");
-		Serial.println(incomingByte);
-               analogWrite(9, incomingByte);
-	}
+    dist_right = leftEncoder.read();
+    dist_left = rightEncoder.read();
 
-     // left
+    dist = (dist_left + dist_right) / 2;
+    angle = dist_right - dist_left;
 
+    err_dist = target_dist - dist;
+    err_angle = target_angle - angle;
+
+    cmd_dist = maximize(err_dist, 50);
+    cmd_angle = maximize(err_angle, 50);
+
+    cmd_right = cmd_dist + cmd_angle;
+    cmd_left = cmd_dist - cmd_angle;
+
+    if (cmd_right > 0) {
+        analogWrite(9, cmd_right);
+        digitalWrite(11, LOW);
+    } else {
+        analogWrite(9, -cmd_right);
+        digitalWrite(11, HIGH);
+    }
+
+    if (cmd_left > 0) {
+        analogWrite(10, cmd_left);
+        digitalWrite(12, HIGH);
+    } else {
+        analogWrite(10, -cmd_left);
+        digitalWrite(12, LOW);
+    }
+}
+
+int32_t maximize(int32_t value, int32_t max)
+{
+    if(max == -1) return value;
+
+    if (abs(value) > max) {
+        if(value > 0) {
+            return max;
+        } else {
+            return -max;
+        }
+    }
+    return value;
+}
+
+void litEntierLong(long int *i)
+{
+    long int aux = 0;
+    unsigned char j;
+    long int k = 10000000;
+    unsigned char c = 0; // les unsigned feront l'affaire ! -- Yann Sionneau
+    for (j = 0; j < 8; j++) {
+        while (Serial.available()==0) { 
+            asm("nop");
+        }
+        c = Serial.read();
+        if (c < 48 || c > 57) {
+            aux = -1;
+            break;
+        }
+        aux += (c - 48) * k;
+        k /= 10;
+    } 
+    *i = aux;
 }
